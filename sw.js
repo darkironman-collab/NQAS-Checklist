1e1b3902a580cdc9037b977a755de6aadead6c0d
darkironman-collab <darkironman@kpgs.xyz>
2026-08-17 14:55:07 +0530

    Cache complete compressed checklist for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aamsc-nqas-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./data/meta.json','./data/checkpoints-01.json','./data/checkpoints-02.json','./data/checkpoints-03.json','./data/checkpoints-04.json','./data/checkpoints-05.json','./data/checkpoints-06.json','./data/checkpoints-07.json','./data/checkpoints-08.json','./data/checkpoints-09.json','./data/checkpoints-10.json','./data/checkpoints-11.json'];
+const CACHE='aamsc-nqas-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./data/meta.json','./data/part-01.txt','./data/part-02.txt','./data/part-03.txt','./data/part-04.txt','./data/part-05.txt','./data/part-06.txt','./data/part-07.txt','./data/part-08.txt','./data/part-09.txt','./data/part-10.txt','./data/part-11.txt','./data/part-12.txt','./data/part-13.txt','./data/part-14.txt','./data/part-15.txt'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
