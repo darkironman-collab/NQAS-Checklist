b6e20b2e13f867c13dcd698535c32d22b87f44b5
darkironman-collab <darkironman@kpgs.xyz>
2026-08-17 15:17:58 +0530

    Update service worker for PWA install and offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aamsc-nqas-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./data/meta.json','./data/part-01.txt','./data/part-02.txt','./data/part-03.txt','./data/part-04.txt','./data/part-05.txt','./data/part-06.txt','./data/part-07.txt','./data/part-08.txt','./data/part-09.txt','./data/part-10.txt','./data/part-11.txt','./data/part-12.txt','./data/part-13.txt','./data/part-14.txt','./data/part-15.txt'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='aamsc-nqas-pwa-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg','./data/meta.json','./data/part-01.txt','./data/part-02.txt','./data/part-03.txt','./data/part-04.txt','./data/part-05.txt','./data/part-06.txt','./data/part-07.txt','./data/part-08.txt','./data/part-09.txt','./data/part-10.txt','./data/part-11.txt','./data/part-12.txt','./data/part-13.txt','./data/part-14.txt','./data/part-15.txt'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))) });
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res}).catch(()=>caches.match('./index.html'))))});
